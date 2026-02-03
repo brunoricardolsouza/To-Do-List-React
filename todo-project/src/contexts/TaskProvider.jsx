d05c1524af73a9ccf0ce58935167bb8c18d99c8a
Bruno Ricardo <brunoricardolsouza@gmail.com>
2026-02-03 18:28:33 -0300

    local location storage  has been added to the project
    in this commit, a local storage has been added to the project in order not to lose tasks already added

--- todo-project/src/contexts/TaskProvider.jsx
+++ todo-project/src/contexts/TaskProvider.jsx
@@ -1,10 +1,18 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { TaskContext } from "./TaskContext";
 
 export const TaskProvider = ({ children }) => {
   //States
   const [taskText, setTaskText] = useState("");
-  const [listTasks, setListTasks] = useState([]);
+
+  const [listTasks, setListTasks] = useState(() => {
+    const stored = localStorage.getItem("tasks");
+    return stored ? JSON.parse(stored) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(listTasks));
+  }, [listTasks]);
 
   const value = useMemo(
     () => ({
